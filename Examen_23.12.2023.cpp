1ada6a9c3fd0c910cf91b67ca855c040dae852ad
Nikita <draco1997pst@gmail.com>
2023-12-23 12:02:01 +0200

    add class Animal and full metods

--- Examen_23.12.2023.cpp
+++ Examen_23.12.2023.cpp
@@ -4,28 +4,105 @@
 
 using namespace std;
 
-
+// baise class Animall
 class Animal{
 private:
     string name;
-    int heilng;
-    int food;
+    int healing = 100;
+    int food = 100;
 public:
-    Animal(string name, int heiling, int food) {
+    Animal(string name) {
         this->name = name;
-        this->heilng = heiling;
-        this->food = food;
+        
     }
+
+    string getName() { return name;}
+    void setName(string name) { this->name = name; }
+
+    int getHealing() { return healing; }
+
+    void addHealing(int healing) {
+        if (getHealing() > 0) {
+            if (getHealing() + healing >= 100) {
+                this->healing = 100;
+            }
+            else if (getHealing() + healing < 100 and getHealing() + healing >= 0) {
+                this->healing += healing;
+            }
+            else { this->healing = 0; }
+        }
+        else
+        {
+            cout << "Animal " << getName() << " is DEAD!" << endl;
+            
+        }
+
+       
+
+    }
+
+    int getFood() { return food; }
+
+    void addFood(int food) {
+        if (getHealing() > 0) {
+            if (getFood() + food >= 100) {
+                this->food = 100;
+            }
+            else if (getFood() + food < 100 and getFood() + food >= 0) {
+                this->food += food;
+            }
+            else { this->food = 0; }
+        }
+        else
+        {
+            cout << "Animal " << getName() << " is DEAD! Is NOT HUNGRY!!((" << endl;
+
+        }
+        
+    }
+
+
 
 };
     
 
+
+class Emploue {
+private:
+    string name;
+    int age;
+
+};
 
 
 
 
 void main()
 {
-    
+    /*Animal cat("jony");
+
+    cout <<cat.getName()<< endl;
+    cout<<cat.getHealing()<<endl;
+    cat.addHealing(-50);
+    cout << cat.getHealing() << endl;
+    cat.addHealing(20);
+    cout << cat.getHealing() << endl;
+    cat.addHealing(-710);
+
+    cout << cat.getHealing() << endl;
+    cat.addHealing(2);
+    cout << cat.getHealing() << endl;
+
+    cout << "FOOD "<< cat.getFood() << endl;
+
+    cat.addFood(20);
+    cout << "FOOD " << cat.getFood() << endl;
+
+    cat.addFood(-40);
+    cout << "FOOD " << cat.getFood() << endl;
+    cat.addFood(-60);
+    cout << "FOOD " << cat.getFood() << endl;*/
+
+
 }
 
