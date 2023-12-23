97d472e86b6b885ea69236a3af1ff1935566dd4a
Nikita <draco1997pst@gmail.com>
2023-12-23 11:04:16 +0200

    Добавьте файлы проекта.

---
+++ Examen_23.12.2023.cpp
@@ -0,0 +1,31 @@
+﻿//Разработка системы управления виртуальным зоопарком
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Animal{
+private:
+    string name;
+    int heilng;
+    int food;
+public:
+    Animal(string name, int heiling, int food) {
+        this->name = name;
+        this->heilng = heiling;
+        this->food = food;
+    }
+
+};
+    
+
+
+
+
+
+void main()
+{
+    
+}
+
